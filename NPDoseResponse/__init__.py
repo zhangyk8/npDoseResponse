4be419770498f449209fff355b1873bee6578c18
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2024-05-13 12:25:32 -0700

    Delete NPDoseResponse directory

--- NPDoseResponse/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Nonparametric Estimation and Inference on Dose-Response Curves"""
-
-from .NPDoseResponse import IntegEst, DerivEffect, LocalPolyReg, LocalPolyReg1D, RegAdjust
-
-__author__ = "Yikun Zhang"
-__version__ = "0.0.2"
