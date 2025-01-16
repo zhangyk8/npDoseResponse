56a1ecfcd863de674437edc1041ad5a1b40f6578
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2025-01-15 16:49:54 -0800

    Add files via upload

--- Paper_Code/Linear_Confounding_Model.py
+++ Paper_Code/Linear_Confounding_Model.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 @author: Yikun Zhang
-Last Editing: April 27, 2024
+Last Editing: January 6, 2025
 
 Bootstrap inference on the dose-response curve by our integral estimator and 
 traditional regression adjustment estimator under the linear confounding model.
@@ -19,59 +19,59 @@ print(job_id)
 #=======================================================================================#
 
 # Linear confounding model
-n = 2000
-np.random.seed(123)
-S3 = np.concatenate([2*np.random.rand(n).reshape(-1,1) - 1, 
-                     2*np.random.rand(n).reshape(-1,1) - 1], axis=1)
-E3 = np.random.rand(n) - 0.5
-T3 = 2*S3[:,0] + S3[:,1] + E3
-Y3 = T3 + 6*S3[:,0] + 6*S3[:,1] + np.random.normal(loc=0, scale=1, size=n)
-X3 = np.concatenate([T3.reshape(-1,1), S3], axis=1)
+for n in [500, 1000, 2000, 5000]:
+    np.random.seed(123)
+    S3 = np.concatenate([2*np.random.rand(n).reshape(-1,1) - 1, 
+                         2*np.random.rand(n).reshape(-1,1) - 1], axis=1)
+    E3 = np.random.rand(n) - 0.5
+    T3 = 2*S3[:,0] + S3[:,1] + E3
+    Y3 = T3 + 6*S3[:,0] + 6*S3[:,1] + np.random.normal(loc=0, scale=1, size=n)
+    X3 = np.concatenate([T3.reshape(-1,1), S3], axis=1)
 
-# Estimate the dose-response curve and its derivative 
-t_qry3 = np.linspace(min(T3)+0.01, max(T3)-0.01, 200)
+    # Estimate the dose-response curve and its derivative 
+    t_qry3 = np.linspace(min(T3)+0.01, max(T3)-0.01, 200)
 
-if job_id == 1:
-    h_cur3, b_cur3 = RoTBWLocalPoly(Y3, X3, kernT="epanechnikov", kernS="epanechnikov")
+    if job_id == 1:
+        h_cur3, b_cur3 = RoTBWLocalPoly(Y3, X3, kernT="epanechnikov", kernS="epanechnikov")
 
-    theta_est3 = DerivEffect(Y3, X3, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
-                h=h_cur3, b=b_cur3, degree=2, deriv_ord=1, kernT="epanechnikov", kernS="epanechnikov")
-
-    m_est3 = IntegEst(Y3, X3, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
+        theta_est3 = DerivEffect(Y3, X3, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
                     h=h_cur3, b=b_cur3, degree=2, deriv_ord=1, kernT="epanechnikov", kernS="epanechnikov")
 
-    Y_RA3 = RegAdjust(Y3, X3, t_eval=t_qry3, degree=2, deriv_ord=0, h=h_cur3, b=b_cur3, 
+        m_est3 = IntegEst(Y3, X3, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
+                        h=h_cur3, b=b_cur3, degree=2, deriv_ord=1, kernT="epanechnikov", kernS="epanechnikov")
+
+        Y_RA3 = RegAdjust(Y3, X3, t_eval=t_qry3, degree=2, deriv_ord=0, h=h_cur3, b=b_cur3, 
+                         kernT="epanechnikov", kernS="epanechnikov")
+
+        Y_RA_deriv3 = RegAdjust(Y3, X3, t_eval=t_qry3, degree=2, deriv_ord=1, h=h_cur3, b=b_cur3,
+                         kernT="epanechnikov", kernS="epanechnikov")
+
+
+    # Bootstrap the original data
+    np.random.seed(job_id)
+    ind = np.random.choice(n, size=n, replace=True)
+    X3_boot = X3[ind,:]
+    Y3_boot = Y3[ind]
+
+    h_boot3, b_boot3 = RoTBWLocalPoly(Y3_boot, X3_boot, kernT="epanechnikov", kernS="epanechnikov")
+
+    # Estimate the dose-response curve and its derivative on the bootstrapping data
+    theta_est3_boot = DerivEffect(Y3_boot, X3_boot, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
+                                 h=h_boot3, b=b_boot3, degree=2, deriv_ord=1, kernT="epanechnikov", 
+                                 kernS="epanechnikov")
+
+    m_est3_boot = IntegEst(Y3_boot, X3_boot, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
+                            h=h_boot3, b=b_boot3, degree=2, deriv_ord=1, kernT="epanechnikov", kernS="epanechnikov")
+
+    Y_RA3_boot = RegAdjust(Y3_boot, X3_boot, t_eval=t_qry3, degree=2, deriv_ord=0, h=h_boot3, b=b_boot3, 
                      kernT="epanechnikov", kernS="epanechnikov")
 
-    Y_RA_deriv3 = RegAdjust(Y3, X3, t_eval=t_qry3, degree=2, deriv_ord=1, h=h_cur3, b=b_cur3,
+    Y_RA_deriv3_boot = RegAdjust(Y3_boot, X3_boot, t_eval=t_qry3, degree=2, deriv_ord=1, h=h_boot3, b=b_boot3,
                      kernT="epanechnikov", kernS="epanechnikov")
 
-
-# Bootstrap the original data
-np.random.seed(job_id)
-ind = np.random.choice(n, size=n, replace=True)
-X3_boot = X3[ind,:]
-Y3_boot = Y3[ind]
-
-h_boot3, b_boot3 = RoTBWLocalPoly(Y3_boot, X3_boot, kernT="epanechnikov", kernS="epanechnikov")
-
-# Estimate the dose-response curve and its derivative on the bootstrapping data
-theta_est3_boot = DerivEffect(Y3_boot, X3_boot, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
-                             h=h_boot3, b=b_boot3, degree=2, deriv_ord=1, kernT="epanechnikov", 
-                             kernS="epanechnikov")
-
-m_est3_boot = IntegEst(Y3_boot, X3_boot, t_eval=t_qry3, h_bar=None, kernT_bar="gaussian", 
-                        h=h_boot3, b=b_boot3, degree=2, deriv_ord=1, kernT="epanechnikov", kernS="epanechnikov")
-
-Y_RA3_boot = RegAdjust(Y3_boot, X3_boot, t_eval=t_qry3, degree=2, deriv_ord=0, h=h_boot3, b=b_boot3, 
-                 kernT="epanechnikov", kernS="epanechnikov")
-
-Y_RA_deriv3_boot = RegAdjust(Y3_boot, X3_boot, t_eval=t_qry3, degree=2, deriv_ord=1, h=h_boot3, b=b_boot3,
-                 kernT="epanechnikov", kernS="epanechnikov")
-
-if job_id == 1:
-    with open('./Results_New/Linear_Conf_Bootstrap_'+str(job_id)+'_new.dat', "wb") as file:
-        pickle.dump([theta_est3, m_est3, Y_RA3, Y_RA_deriv3, theta_est3_boot, m_est3_boot, Y_RA3_boot, Y_RA_deriv3_boot], file)
-else:
-    with open('./Results_New/Linear_Conf_Bootstrap_'+str(job_id)+'_new.dat', "wb") as file:
-        pickle.dump([theta_est3_boot, m_est3_boot, Y_RA3_boot, Y_RA_deriv3_boot], file)
+    if job_id == 1:
+        with open('./Results_New/Linear_Conf_Bootstrap_'+str(job_id)+'_new_bw_n'+str(n)+'.dat', "wb") as file:
+            pickle.dump([theta_est3, m_est3, Y_RA3, Y_RA_deriv3, theta_est3_boot, m_est3_boot, Y_RA3_boot, Y_RA_deriv3_boot], file)
+    else:
+        with open('./Results_New/Linear_Conf_Bootstrap_'+str(job_id)+'_new_bw_n'+str(n)+'.dat', "wb") as file:
+            pickle.dump([theta_est3_boot, m_est3_boot, Y_RA3_boot, Y_RA_deriv3_boot], file)
--- Paper_Code/Single_Confounder_Model.py
+++ Paper_Code/Single_Confounder_Model.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 @author: Yikun Zhang
-Last Editing: April 27, 2024
+Last Editing: January 6, 2025
 
 Bootstrap inference on the dose-response curve by our integral estimator and 
 traditional regression adjustment estimator under the single confounder model.
@@ -19,62 +19,64 @@ print(job_id)
 #=======================================================================================#
 
 # Single confounder model
-n = 2000
-np.random.seed(123)
-S1 = 2*np.random.rand(n) - 1
-E = np.random.rand(n)*0.6 - 0.3
-T1 = np.sin(np.pi*S1) + E
-Y1 = T1**2 + T1 + 1 + 10*S1 + np.random.normal(loc=0, scale=1, size=n)
-X1 = np.concatenate([T1.reshape(-1,1), S1.reshape(-1,1)], axis=1)
+for n in [500, 1000, 2000, 5000]:
+    np.random.seed(123)
+    S1 = 2*np.random.rand(n) - 1
+    E = np.random.rand(n)*0.6 - 0.3
+    T1 = np.sin(np.pi*S1) + E
+    Y1 = T1**2 + T1 + 1 + 10*S1 + np.random.normal(loc=0, scale=1, size=n)
+    X1 = np.concatenate([T1.reshape(-1,1), S1.reshape(-1,1)], axis=1)
 
-# Estimate the dose-response curve and its derivative 
-t_qry1 = np.linspace(min(T1)+0.01, max(T1)-0.01, 200)
+    # Estimate the dose-response curve and its derivative 
+    t_qry1 = np.linspace(min(T1)+0.01, max(T1)-0.01, 200)
+    
+    para = False
 
-if job_id == 1:
-    h_cur1, b_cur1 = RoTBWLocalPoly(Y1, X1, kernT="epanechnikov", kernS="epanechnikov")
+    if job_id == 1:
+        h_cur1, b_cur1 = RoTBWLocalPoly(Y1, X1, kernT="epanechnikov", kernS="epanechnikov")
 
-    theta_est1 = DerivEffect(Y1, X1, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
-                             h=h_cur1, b=b_cur1, degree=2, deriv_ord=1, kernT="epanechnikov", 
-                             kernS="epanechnikov")
+        theta_est1 = DerivEffect(Y1, X1, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
+                                 h=h_cur1, b=b_cur1, degree=2, deriv_ord=1, kernT="epanechnikov", 
+                                 kernS="epanechnikov", parallel=para, processes=60)
 
-    m_est1 = IntegEst(Y1, X1, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
-                        h=h_cur1, b=b_cur1, degree=2, deriv_ord=1, kernT="epanechnikov", 
-                        kernS="epanechnikov")
+        m_est1 = IntegEst(Y1, X1, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
+                            h=h_cur1, b=b_cur1, degree=2, deriv_ord=1, kernT="epanechnikov", 
+                            kernS="epanechnikov", parallel=para, processes=60)
 
-    Y_RA1 = RegAdjust(Y1, X1, t_eval=t_qry1, degree=2, deriv_ord=0, h=h_cur1, b=b_cur1, 
-                     kernT="epanechnikov", kernS="epanechnikov")
+        Y_RA1 = RegAdjust(Y1, X1, t_eval=t_qry1, degree=2, deriv_ord=0, h=h_cur1, b=b_cur1, 
+                         kernT="epanechnikov", kernS="epanechnikov", parallel=para, processes=60)
 
-    Y_RA_deriv1 = RegAdjust(Y1, X1, t_eval=t_qry1, degree=2, deriv_ord=1, h=h_cur1, b=b_cur1,
-                     kernT="epanechnikov", kernS="epanechnikov")
+        Y_RA_deriv1 = RegAdjust(Y1, X1, t_eval=t_qry1, degree=2, deriv_ord=1, h=h_cur1, b=b_cur1,
+                         kernT="epanechnikov", kernS="epanechnikov", parallel=para, processes=60)
 
 
-# Bootstrap the original data
-np.random.seed(job_id)
-ind = np.random.choice(n, size=n, replace=True)
-X1_boot = X1[ind,:]
-Y1_boot = Y1[ind]
+    # Bootstrap the original data
+    np.random.seed(job_id)
+    ind = np.random.choice(n, size=n, replace=True)
+    X1_boot = X1[ind,:]
+    Y1_boot = Y1[ind]
 
-h_boot1, b_boot1 = RoTBWLocalPoly(Y1_boot, X1_boot, kernT="epanechnikov", kernS="epanechnikov")
+    h_boot1, b_boot1 = RoTBWLocalPoly(Y1_boot, X1_boot, kernT="epanechnikov", kernS="epanechnikov")
 
-# Estimate the dose-response curve and its derivative on the bootstrapping data
-theta_est1_boot = DerivEffect(Y1_boot, X1_boot, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
+    # Estimate the dose-response curve and its derivative on the bootstrapping data
+    theta_est1_boot = DerivEffect(Y1_boot, X1_boot, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
+                                 h=h_boot1, b=b_boot1, degree=2, deriv_ord=1, kernT="epanechnikov", 
+                                 kernS="epanechnikov", parallel=para, processes=60)
+
+    m_est1_boot = IntegEst(Y1_boot, X1_boot, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
                              h=h_boot1, b=b_boot1, degree=2, deriv_ord=1, kernT="epanechnikov", 
-                             kernS="epanechnikov")
+                             kernS="epanechnikov", parallel=para, processes=60)
 
-m_est1_boot = IntegEst(Y1_boot, X1_boot, t_eval=t_qry1, h_bar=None, kernT_bar="gaussian", 
-                         h=h_boot1, b=b_boot1, degree=2, deriv_ord=1, kernT="epanechnikov", 
-                         kernS="epanechnikov")
+    Y_RA1_boot = RegAdjust(Y1_boot, X1_boot, t_eval=t_qry1, degree=2, deriv_ord=0, h=h_boot1, b=b_boot1, 
+                     kernT="epanechnikov", kernS="epanechnikov", parallel=para, processes=60)
 
-Y_RA1_boot = RegAdjust(Y1_boot, X1_boot, t_eval=t_qry1, degree=2, deriv_ord=0, h=h_boot1, b=b_boot1, 
-                 kernT="epanechnikov", kernS="epanechnikov")
+    Y_RA_deriv1_boot = RegAdjust(Y1_boot, X1_boot, t_eval=t_qry1, degree=2, deriv_ord=1, h=h_boot1, b=b_boot1,
+                     kernT="epanechnikov", kernS="epanechnikov", parallel=para, processes=60)
 
-Y_RA_deriv1_boot = RegAdjust(Y1_boot, X1_boot, t_eval=t_qry1, degree=2, deriv_ord=1, h=h_boot1, b=b_boot1,
-                 kernT="epanechnikov", kernS="epanechnikov")
-
-if job_id == 1:
-    with open('./Results_New/Single_Conf_Bootstrap_'+str(job_id)+'_new.dat', "wb") as file:
-        pickle.dump([theta_est1, m_est1, Y_RA1, Y_RA_deriv1, theta_est1_boot, m_est1_boot, Y_RA1_boot, Y_RA_deriv1_boot], file)
-else:
-    with open('./Results_New/Single_Conf_Bootstrap_'+str(job_id)+'_new.dat', "wb") as file:
-        pickle.dump([theta_est1_boot, m_est1_boot, Y_RA1_boot, Y_RA_deriv1_boot], file)
+    if job_id == 1:
+        with open('./Results_New/Single_Conf_Bootstrap_'+str(job_id)+'_new_bw_n'+str(n)+'.dat', "wb") as file:
+            pickle.dump([theta_est1, m_est1, Y_RA1, Y_RA_deriv1, theta_est1_boot, m_est1_boot, Y_RA1_boot, Y_RA_deriv1_boot], file)
+    else:
+        with open('./Results_New/Single_Conf_Bootstrap_'+str(job_id)+'_new_bw_n'+str(n)+'.dat', "wb") as file:
+            pickle.dump([theta_est1_boot, m_est1_boot, Y_RA1_boot, Y_RA_deriv1_boot], file)
 
